b80c569257b94884cec7d1cc27759312d1ccefb3
danielfahg <df.hernandez12@uniandes.edu.co>
2018-10-23 09:36:49 -0500

    agrego archivos .dat y .png que faltaban

--- Fourier.py
+++ Fourier.py
@@ -12,4 +12,7 @@ print("por favor revisar este punto con python 2")
 #https://www.kaggle.com/questions-and-answers/27699
 #https://docs.scipy.org/doc/numpy-1.15.1/reference/generated/numpy.fromfile.html
 
+#genformtxt funciona segun la profesora para el .dat
+#ayuda obedecer a la termianl su sugerencia para lo de git
+
 
