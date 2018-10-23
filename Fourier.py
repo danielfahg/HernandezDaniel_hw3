43ea54142fb5b6ed8f8387175048da1d8452afca
danielfahg <df.hernandez12@uniandes.edu.co>
2018-10-23 08:54:24 -0500

    con links para punto 3.1

--- Fourier.py
+++ Fourier.py
@@ -1,4 +1,15 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-print("por favor revisar este putno con python 2")
+print("por favor revisar este punto con python 2")
+
+#1
+
+#links de investigacion>
+#https://www.google.com/search?client=ubuntu&channel=fs&q=read+a+.dat+python&ie=utf-8&oe=utf-8
+#https://stackoverflow.com/questions/11798800/reading-a-binary-dat-file-as-an-array
+#https://stackoverflow.com/questions/49329010/how-to-decode-a-dat-file-in-python
+#https://www.kaggle.com/questions-and-answers/27699
+#https://docs.scipy.org/doc/numpy-1.15.1/reference/generated/numpy.fromfile.html
+
+
