f80603da9c304110b47a4881f0ed4aeec4032dd1
danielfahg <df.hernandez12@uniandes.edu.co>
2018-10-31 00:14:26 -0400

    inicio de punto 4

---
+++ Fourier2D.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pylab as plt
+import scipy
+
+arrIma=scipy.misc.imread("arbol.png")
+print("arrIma", arrIma)
+
+#https://pypi.org/project/imread/ puede ayudar a que funcione el metodo, o que debo instlarle a mi computador
